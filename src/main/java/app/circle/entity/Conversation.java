00b57f9abd899b3c64651add25eade27b52884e3
burakozturk <burak.ozturk@biligoba.com>
2023-12-29 14:01:57 +0300

    Removed Command Line

--- src/main/java/app/circle/entity/Conversation.java
+++ src/main/java/app/circle/entity/Conversation.java
@@ -1,37 +1,37 @@
-//package app.circle.entity;
-//
-//import jakarta.persistence.*;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//import java.time.LocalDateTime;
-//import java.util.List;
-//import java.util.UUID;
-//
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Entity
-//@Table(name = "conversation")
-//public class Conversation {
-//
-//    @Id
-//    @GeneratedValue
-//    private UUID id;
-//
-//    @Enumerated(EnumType.STRING)
-//    private ConversationType type;
-//
-//    @ManyToMany(fetch = FetchType.LAZY)
-//    @JoinTable(name = "conversation_user",
-//            joinColumns = @JoinColumn(name = "conversation_id"),
-//            inverseJoinColumns = @JoinColumn(name = "user_id"))
-//    private List<User> participants;
-//
-//    private LocalDateTime createdAt = LocalDateTime.now();
-//
-//    @OneToMany(mappedBy = "conversation", cascade = CascadeType.ALL)
-//    private List<Message> messages;
-//
-//}
+package app.circle.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "conversation")
+public class Conversation {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Enumerated(EnumType.STRING)
+    private ConversationType type;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "conversation_user",
+            joinColumns = @JoinColumn(name = "conversation_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private List<User> participants;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @OneToMany(mappedBy = "conversation", cascade = CascadeType.ALL)
+    private List<Message> messages;
+
+}
